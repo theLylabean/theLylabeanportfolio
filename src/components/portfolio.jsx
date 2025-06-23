c7742a60cafd036dcb59d958f2ff0f98082c5bad
Lyla Lynn <lyladlynn@gmail.com>
2025-06-23 15:40:28 -0500

    editing the portfolio part of the website

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -12,7 +12,7 @@ const Portfolio = () => {
                 <section>
                     <h3>🛡️<u>RealmForge</u></h3>
                     <p>
-                        My most recent project has been my D&D Character Builder. Right now, it's a very rudimentary step-by-step guide to build a D&D character. As I update this app, I will be adding more complex functions like:
+                        My most recent front-end dev project has been my D&D Character Builder. Right now, it's a very rudimentary step-by-step guide to build a D&D character. As I update this app, I will be adding more complex functions like:
                     </p>
                     <ol> 
                         <li>Account creation with login, account info page, and a section to show saved Characters. </li>
@@ -40,9 +40,28 @@ const Portfolio = () => {
                 <section>
                     <h3><u>Back-end Development</u></h3>
                     <p>
-                        Hello!
+                        My most recent back-end dev project has been a task list that users can register for. There's authentication and users who are authenticated can view, add, delete, and update tasks for themselves! In this project I learned things like:
                     </p>
-                    <img className='image' src={ boop } />
+                    <ol> 
+                        <li> Account creation with login, account info page, and a section to show saved Characters. </li>
+                        <li> Pulling equpitment from the API. </li>
+                        <li> Highlighting recommended stats, spells, and equiptment for level 1 characters based on class. </li>
+                        <li> The ability to export to pdf and print. </li>
+                    </ol>
+                    <p>
+                        This project started as an extra credit assignment for class and we were given the choice to create anything we wanted. I already had several smaller projects built that could be used for this assignment; however, I decided to really dive deep in to learning new concepts to build something I'm passionate about. Some new concepts I learned at the time:
+                    </p>
+                    <ul>
+                        <li> Step counter to use for filling out forms, making them more seemless. </li>
+                        <li> Using Javascript to capitalize all words except 'and', 'or', 'the', etc. </li>
+                        <li> Using Javascript to set explicit requirements for password creation along with error messages for each step based on what requirement is missing. </li>
+                        <li> How to give an error message if a class was not selected and give a message if there are no spells found for that class. I also included using some method chaining (.charAt(0).toUpperCase and .slice(1)) to make sure the class name is capitalized. </li>
+                    </ul>
+                    <div className='imgrow'>
+                        <img className='image' src={ home } />
+                        <img className='image' src={ details } />
+                        <img className='image' src={ final } />
+                    </div>
                 </section>
             </main>
             <main>
