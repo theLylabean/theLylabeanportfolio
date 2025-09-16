8324506892abf215736a548394ae8442c3aa3048
Lyla Lynn <lyladlynn@gmail.com>
2025-09-16 16:55:41 -0500

    updating portfolio again

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './css/App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home.jsx';
-import Portfolio from './components/Portfolio.jsx';
+import Portfolio from './components/portfolio.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 import Aboutme from './components/Aboutme.jsx'
